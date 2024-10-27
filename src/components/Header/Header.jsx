0d43cba38a6fe227525e47257544e31177c2cb5b
IMLUOAI <IMLUOAI@gmail.com>
2024-10-26 23:14:57 -0500

    Updated css for all of the resolutions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,44 @@
 import "../Header/header.css";
 import Navigation from "..//Navigation/Navigation";
+import { useState } from "react";
+import menuIcon from "../../images/menu.svg";
+import crossIcon from "../../images/back.svg";
 
-const Header = ({
-  userName = "",
-  isAuthorized,
-  onLogout,
-  onSigninModal
-}) => {
+const Header = ({ userName = "", isAuthorized, onLogout, onSigninModal }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className="header__bar">
         <h1 className="header__logo" alt="logo">
           NewsExplorer
         </h1>
-       <Navigation
-       isLoggedIn={isAuthorized}
-       userName={userName}
-       onLogout={onLogout}
-       onSigninModal={onSigninModal}
-       />
+        <button
+          className="header__menu-button"
+          type="button"
+          onClick={toggleMenu}
+        >
+          <img src={menuIcon} className="header__menu-icon" alt="menu-icon" />
+        </button>
+        <Navigation
+          isLoggedIn={isAuthorized}
+          userName={userName}
+          onLogout={onLogout}
+          onSigninModal={onSigninModal}
+          handleCloseMenu={toggleMenu}
+        />
+        {isMenuOpen && (
+          <div className="header__mobile-menu" onClick={toggleMenu}>
+            <button className="header__close-icon">
+              <img src={crossIcon} alt="close" />
+            </button>
+            <a href="/">Home</a>
+            <button onClick={onSigninModal}>Sign in</button>
+          </div>
+        )}
       </div>
     </header>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -15,15 +15,13 @@ const Main = ({
   console.log("newsData", newsData);
   return (
     <main className="main__section">
-      <div className="main__container">
-        <div className="main__content">
-          <h1 className="main__title">What's going on in the world?</h1>
-          <h3 className="main__description">
-            Find the latest news on any topic and save them in your personal
-            accout
-          </h3>
-          <SearchBar onSearch={handleSearch} />
-        </div>
+      <div className="main__content">
+        <h1 className="main__title">What's going on in the world?</h1>
+        <h3 className="main__description">
+          Find the latest news on any topic and save them in your personal
+          accout
+        </h3>
+        <SearchBar onSearch={handleSearch} />
       </div>
       {hasSearched && (
         <div className="news__section">
